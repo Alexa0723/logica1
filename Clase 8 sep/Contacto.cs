45eb2ac75c8f8258301eed0e28f9077ad63b2c60
Alexa0723 <liceth.sarmiento@udea.edu.co>
2023-10-05 19:46:52 -0500

    feat: subi el archivo

---
+++ Clase 8 sep/Contacto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_8_sep
+{
+    internal class Contacto
+    {
+        String nombreCompleto;
+        String telefono;
+        String correo;
+        String genero;
+        String cumple;
+        int edad; 
+        
+        public Contacto(String pNombreCompleto, String pTelefono, String pCorreo, String pGenero, String pCumple, int pEdad)
+        {       
+            nombreCompleto = pNombreCompleto;
+            telefono = pTelefono;
+            correo = pCorreo;
+            genero = pGenero;
+            cumple = pCumple;
+            edad = pEdad;   
+        }
+        public String mostrarDatos() 
+        {
+            String texto = "\nNombre completo:  " + nombreCompleto + "\nTelefono: " + telefono + "\nCorreo: " + correo + "\nGenero: " + genero +
+                "\nCumple: " + cumple +"\nEdad: " + edad;
+            return texto;
+        }
+    
+    }
+}
---
+++ Clase 8 sep/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_8_sep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Holii");
+            Console.Write("Ingresa su nombre:");
+            String nombre = Console.ReadLine();
+            Console.Write("Ingresa su telefono:");
+            String telefono = Console.ReadLine();
+            Console.Write("Ingresa su correo:");
+            String correo = Console.ReadLine();
+            Console.Write("Ingresa su genero:");
+            String genero = Console.ReadLine();
+            Console.Write("Ingrese su cumpleaños:");
+            String cumple = Console.ReadLine();
+            Console.Write("Ingresa su edad:");
+            int edad = int.Parse(Console.ReadLine());
+
+            Contacto contacto1 = new Contacto(nombre, telefono, correo, genero, cumple,edad);
+            Console.WriteLine(contacto1.mostrarDatos());
+
+        }
+    }
+}
